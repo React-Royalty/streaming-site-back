eeb16d24f457cdc9f70bb9cd6a0eba8c0d24508b
Madi-Webb <madilynwebb98@gmail.com>
2023-03-10 15:36:48 -0700

    wrote media patch function

--- api/media.js
+++ api/media.js
@@ -4,7 +4,7 @@ const express = require("express");
 const mediaRouter = express.Router();
 
 // function imports
-const { getAllMedia, getMediaById, getMediaByTitle, createMedia } = require("../db/media");
+const { getAllMedia, getMediaById, getMediaByTitle, createMedia, updateMedia } = require("../db/media");
 const { requireUser } = require("./utils");
 
 
@@ -12,14 +12,15 @@ const { requireUser } = require("./utils");
 
 /** 
 ** POST /api/media
-* Adds new media to media table in database. Requires an admin user
-* TODO: Requires an admin user
-*/
+ * Adds new media to media table in database. Requires an admin user
+ * @see /db/media/createMedia 
+ * TODO: Require an admin user
+ */
 // mediaRouter.post("/", requireUser, requireAdmin, async (req, res, next) => {
 mediaRouter.post("/", requireUser, async (req, res, next) => {
+  const { title, description, image } = req.body;
+
   try {
-      // console.log(req.body)
-      const { title, description, image } = req.body;
       const newMedia = await createMedia({ title, description, image });
 
       res.send({ 
@@ -34,11 +35,39 @@ mediaRouter.post("/", requireUser, async (req, res, next) => {
 
 
 /** 
-** PATCH /api/media/id
-* Updates media in the media table in the database. Requires an admin user
-* TODO: WRITE
-*/
+ ** PATCH /api/media/id
+ * Updates media in the media table in the database. 
+ * TODO: Require an admin user
+ */
+mediaRouter.patch('/:mediaId', requireUser, async (req, res, next) => {
+  const { mediaId } = req.params;
+  const { title, description, image } = req.body;
 
+  const updateFields={};
+
+  if (title) updateFields.title = natitleme;
+  if (description) updateFields.description = description;
+  if (image) updateFields.image = image;
+
+  try {
+    const media = await updateMedia(mediaId, updateFields);
+
+    if (media) {
+      res.send({
+        success: true,
+        message: "Media Updated",
+        media: media
+      })
+    } else {
+      next ({ 
+        name: "ErrorUpdatingMedia",
+        message: "An error occured while attempting to update media"
+      });
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
 
 
@@ -50,9 +79,10 @@ mediaRouter.post("/", requireUser, async (req, res, next) => {
 
 
 /**
-** GET /api/media
-* Sends back a list of all media in the database
-*/
+ ** GET /api/media
+ * Sends back a list of all media in the database
+ * @see /db/media/getAllMedia 
+ */
 mediaRouter.get("/", async (req, res, next) => {
   try {
     const allMedia = await getAllMedia();
@@ -68,9 +98,10 @@ mediaRouter.get("/", async (req, res, next) => {
 
 
 /**
-** GET /api/media/:id
-* Finds and sends back a media object by its ID
-*/
+ ** GET /api/media/:id
+ * Finds and sends back a media object by its ID
+ * @see /db/media/getMediaById 
+ */
 mediaRouter.get("/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
@@ -86,9 +117,10 @@ mediaRouter.get("/:id", async (req, res, next) => {
 
 
 /**
-** GET /api/media/title/:title 
-* Finds and sends back a media object by its title
-*/
+ ** GET /api/media/title/:title 
+ * Finds and sends back a media object by its title
+ * @see /db/media/getMediaByTitle
+ */
 mediaRouter.get('/title/:title', async (req,res,next) => {
   try{
     const { title } = req.params;
