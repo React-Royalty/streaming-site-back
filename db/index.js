5423563d84edcf8a4f91a70c52ff059e09ced0d9
Madi Webb <mwebb@Madis-MacBook-Pro.local>
2024-02-24 15:06:56 -0700

    testing db for revamp, added bottoms movie

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 require ("dotenv").config();
 const pg = require("pg");
 
-const client = new pg.Client(process.env.DATABASE_URL || "postgres://localhost:5432/streaming-site");
+const client = new pg.Client(process.env.DATABASE_URL || "postgres://localhost:5432/marathon-db");
 
 module.exports = {
   client
